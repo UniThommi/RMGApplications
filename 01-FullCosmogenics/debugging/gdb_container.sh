15fd4d07ad535a7f7ebccc69b66f23dcae541b6f
UniThommi <thomas.buerger@student.uni-tuebingen.de>
2025-05-07 10:15:19 +0200

    Debugger is running

---
+++ 01-FullCosmogenics/debugging/gdb_container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Pfad zur .sif-Datei anpassen!
+apptainer exec /home/tbueger/code/sim/remage02/remage-base.sif gdb "$@"
--- 01-FullCosmogenics/src/HardwareQEOverride.cc
+++ 01-FullCosmogenics/src/HardwareQEOverride.cc
@@ -12,39 +12,39 @@ namespace u = CLHEP;
 // Thanks to Manuel Huber for the suggestion
 // of this method
 G4VPhysicalVolume *HardwareQEOverride::Construct() {
-  auto world = RMGHardware::Construct();
-  // Get the surface of the PMTs
-  auto st = G4SurfaceProperty::GetSurfacePropertyTable();
-  G4SurfaceProperty *s;
-  for (auto x : *st) {
-    if (x->GetName().find("PMTSurface") != std::string::npos)
-      s = x;
-  }
-  // Get the MPT
-  auto mpt = dynamic_cast<G4OpticalSurface *>(s)->GetMaterialPropertiesTable();
+  // auto world = RMGHardware::Construct();
+  // // Get the surface of the PMTs
+  // auto st = G4SurfaceProperty::GetSurfacePropertyTable();
+  // G4SurfaceProperty *s;
+  // for (auto x : *st) {
+  //   if (x->GetName().find("PMTSurface") != std::string::npos)
+  //     s = x;
+  // }
+  // // Get the MPT
+  // auto mpt = dynamic_cast<G4OpticalSurface *>(s)->GetMaterialPropertiesTable();
 
-  // New vector to store the efficiency
-  // This vector might never be deleted, but it should be only generated once so
-  // whatever.
-  G4PhysicsOrderedFreeVector *QuantumEfficiency =
-      new G4PhysicsOrderedFreeVector;
+  // // New vector to store the efficiency
+  // // This vector might never be deleted, but it should be only generated once so
+  // // whatever.
+  // G4PhysicsOrderedFreeVector *QuantumEfficiency =
+  //     new G4PhysicsOrderedFreeVector;
 
-  // Read in the correct quantum efficiency and add to vectors
-  std::ifstream datafile;
-  datafile.open("datasheets/R7081_QEWhitespace.csv");
-  while (datafile.is_open()) {
-    G4double wlen, queff;
+  // // Read in the correct quantum efficiency and add to vectors
+  // std::ifstream datafile;
+  // datafile.open("datasheets/R7081_QEWhitespace.csv");
+  // while (datafile.is_open()) {
+  //   G4double wlen, queff;
 
-    datafile >> wlen >> queff;
+  //   datafile >> wlen >> queff;
 
-    if (datafile.eof())
-      break;
-    // convert the wavelength (supposed to be in nm) to energy
-    QuantumEfficiency->InsertValues(1239.841939 * u::eV / wlen, queff / 100.);
-  }
-  datafile.close();
-  // Overwrite efficiency with new read in efficiency
-  mpt->AddProperty("EFFICIENCY", QuantumEfficiency);
-  // std::cout << (mpt != nullptr) << std::endl;
-  return world;
+  //   if (datafile.eof())
+  //     break;
+  //   // convert the wavelength (supposed to be in nm) to energy
+  //   QuantumEfficiency->InsertValues(1239.841939 * u::eV / wlen, queff / 100.);
+  // }
+  // datafile.close();
+  // // Overwrite efficiency with new read in efficiency
+  // mpt->AddProperty("EFFICIENCY", QuantumEfficiency);
+  // // std::cout << (mpt != nullptr) << std::endl;
+  // return world;
 }
